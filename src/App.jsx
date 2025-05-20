9b916bd00dabb2803c56a6f885f781680e9736b4
003-demo <rohankabir061@gmail.com>
2025-05-21 00:07:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="bg-red-300 text-5xl text-center mt-20">Assignment - 10</h2>
+    </>
+  );
+}
+
+export default App;
