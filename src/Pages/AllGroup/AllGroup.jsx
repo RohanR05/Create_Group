67f893749152a9e74015e1b6caaa43edaee497d7
003-demo <rrriku05@gmail.com>
2025-10-26 23:38:31 +0600

    all group

--- src/Pages/AllGroup/AllGroup.jsx
+++ src/Pages/AllGroup/AllGroup.jsx
@@ -6,8 +6,15 @@ const AllGroup = () => {
   const groupData = useLoaderData();
 
   return (
-    <div >
-      <div className="my-10 md:w-8/12 mx-auto p-2">
+    <div className="mx-auto mt-6 md:mt-10">
+      <h2
+        className="text-xl md:text-3xl font-semibold
+       text-primary text-center"
+      >
+        Here is all <span className="text-secondary">Article</span> posted by
+        all
+      </h2>
+      <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-3">
         {groupData.map((data) => (
           <GroupPage data={data} key={data._id}></GroupPage>
         ))}
